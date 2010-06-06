8427d128d8bdadc276ce19b433f46792b8b3a0d8
stefanfoulis <stefan.foulis@gmail.com>
2010-06-06 17:21:15 +0200

    some fixes

--- cmsplugin_iframe/models.py
+++ cmsplugin_iframe/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from cms.models import CMSPlugin
 
 class IFrame(CMSPlugin):
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=255, null=True, blank=True)
     width = models.PositiveSmallIntegerField(blank=True, null=True)
     height = models.PositiveSmallIntegerField()
     url = models.URLField()
---
+++ metadata.py
@@ -0,0 +1,8 @@
+package_name = 'cmsplugin_iframe'
+name = 'cmsplugin-iframe'
+author = 'Divio GmbH'
+author_email = 'developers@divio.ch'
+description = "A simple django-cms plugin that provides a iframe."
+version = __import__(package_name).__version__
+project_url = 'http://github.com/divio/%s' % name
+license = 'BSD'
